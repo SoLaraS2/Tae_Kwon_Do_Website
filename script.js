737591e4927ce912f27771b34e0cad004dfdadf0
Bezerra <lbezerra@nrel.gov>
2024-10-28 13:35:30 -0600

    Added card

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // script.js
 function showMessage() {
-    alert("Hello from the simplest website!");
+    alert("This is part of my job, so It is trivial. I hope I am of any help to you.");
+  }
+  // script.js
+  
+function flipCard(cardContainer) {
+    cardContainer.classList.toggle('flip');
   }
   
